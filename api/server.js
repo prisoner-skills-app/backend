72320f3cc37b36c14710b4dece437b0c893b6217
tatiana faramarzi <tfaramarzi@gmail.com>
2019-09-24 08:31:20 -0700

    authentication working on register and login

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 
+const authRouter = require('../auth/auth-router.js');
+//const centerRouter = require('../centers/centers-router.js');
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/auth', authRouter);
+//server.use('/api/centers', centerRouter);
+
 
 module.exports = server;
