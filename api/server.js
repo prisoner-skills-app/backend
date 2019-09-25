d394c1747f9d554884f96008e1e31778bdb5ea8a
tatiana faramarzi <tfaramarzi@gmail.com>
2019-09-24 21:38:28 -0700

    Added cors and helmet as dependencies, delete candidate and create candidate endpoints working

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 
 const authRouter = require('../auth/auth-router.js');
 const centerRouter = require('../centers/centers-router.js');
@@ -6,11 +8,16 @@ const candidateRouter = require('../candidates/candidates-router.js');
 
 const server = express();
 
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up and running!' })
+});
 
 server.use('/api/auth', authRouter);
 server.use('/api/centers', centerRouter);
 server.use('/api/candidates', candidateRouter);
-
 
 module.exports = server;
--- centers/centers-router.js
+++ centers/centers-router.js
@@ -5,17 +5,16 @@ const Candidates = require('../candidates/candidates-model.js');
 const restricted = require('../auth/auth-middleware.js');
 
 //PUBLIC OPERATIONS
-
 //get centers with completed profiles
 router.get('/', (req, res) => {
     Centers.findCompleteCenters()
-        .then(centers => {
-            res.status(200).json(centers)
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({ message: 'There was an error retrieving the centers.' })
-        });
+    .then(centers => {
+        res.status(200).json(centers)
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error retrieving the centers.' })
+    });
 });
 
 // get center by id, as well as all candidates from center
@@ -40,6 +39,7 @@ router.get('/:id', (req, res) => {
 });
 
 //AUTH OPERATIONS
+//update a center
 
 
 //delete a center
@@ -54,7 +54,7 @@ router.delete('/:id', (req, res) => {
         .catch(error => {
             console.log(error);
             res.status(404).json({ message: 'No center with this ID exists.'})
-        })
+        });
     })
     .catch(error => {
         console.log(error);
@@ -62,26 +62,63 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+//add a new candidate
+router.post('/:id/candidates', (req, res) => {
+    const newCandidate = req.body;
+    const { id } = req.params;
+
+    Centers.findCenterById(id)
+    .then(center => {
+        if (center) {
+            console.log('center to add candidate to:', center);
+            console.log('candidate to add:', newCandidate);
+            console.log('center id:', id);
+            Candidates.addCandidate(newCandidate, id)
+            .then(candidate => {
+                res.status(201).json(candidate);
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).status({ message: 'There was an error creating a new candidate.' })
+            });
+        } else {
+            res.status(404).json({ message: 'No center with this ID exists.' })
+        }
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error while creating a new candidate profile.' })
+    });
+});
+
+//update a candidate
+
 //delete a candidate
-// router.delete('/:id/candidates/:id', (req, res) => {
-//     const { id } = req.params;
-//     Centers.findCenterById(id)
-//     .then(center => {
-//         const id = center.id;
-//         Centers.findCandidatesByCenterId(id)
-//         .then(persons => {
-//             res.status(200).json({center, persons})
-//         })
-//         .catch(error => {
-//             console.log(error);
-//             res.status(500).json({ message: 'There was an error retrieving the candidates.' })
-//         });
-//     })
-//     .catch(error => {
-//         console.log(error);
-//         res.status(500).json({ message: 'There was an error retrieving the specified center.' })
-//     });
-// });
+router.delete('/:centId/candidates/:candId', (req, res) => {
+    const centId = req.params.centId;
+    const candId = req.params.candId;
+    Candidates.findCandidateById(candId)
+    .then(candidate => {
+        console.log(candidate)
+        if (centId == candidate.centerId){
+            Candidates.removeCandidate(candidate.id)
+            .then(deleted => {
+                res.status(200).json({removed: candidate});
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(404).json({ message: 'No candidate with this ID exists at this center.'});
+            })
+        } else {
+            res.status(404).json({ message: 'You are not authorized to remove this candidate.' }); 
+        }
+        
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'There was an error retrieving the specified candidate.' })
+    });
+});
 
 
 
